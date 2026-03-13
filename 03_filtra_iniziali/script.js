65822f0a62e77dd28987d58d42528b1b959b545c
filippozuppini <zuppini.filippo@gmail.com>
2026-03-13 19:17:25 +0100

    add ex 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -7,9 +7,33 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+//dichiaro la funzione e do in etrata l'array e una lettera
+function trovaNome (array, lettera){
+
+    const askedNames = [];
+    
+    // scorro l'array 
+    for (let i = 0; i < array.length; i++){
+        
+        // se la prima parola inizia per la lettera data in entrata
+        if (array[i][0] === lettera) {
+
+            // push al nuovo array
+            askedNames.push(array[i])
+        }
+
+    }
+
+    return askedNames;
+
+}
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(trovaNome(names, 'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
