01d3d291b86a8a3335d261dd6e9d5a2c2065b11c
filippozuppini <zuppini.filippo@gmail.com>
2026-03-13 19:42:41 +0100

    add ex 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e restituisca
+il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
@@ -6,8 +7,32 @@ const word = 'javascript';
 // Dichiara la funzione qui.
 
 
+// creo la funzione contaVocali dove gli passo in input la stringa in cui cercare
+
+function contaVocali (str){
+    
+    let count = 0;
+
+    //scorriamo la stringa per verificare le sigole lettere
+
+    for (let i = 0; i < str.length; i++){
+
+        //se nella stinga solo presenti A oppure E oppure ecc.. allora incremento di 1
+        if (str[i]=== 'a' || str[i]=== 'e' ||str[i]=== 'i' ||str[i]=== 'o' ||str[i]=== 'u') {
+
+            count++
+            
+        }   
+
+    }
+
+    return count;
+
+}
+
+
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(contaVocali(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
