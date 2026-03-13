98931cdc59cf8c664761a7875de62d69f5ab9239
filippozuppini <zuppini.filippo@gmail.com>
2026-03-13 18:24:43 +0100

    add ex 02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca
+nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
@@ -6,7 +7,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 
 
+function trovaIniziali(array) {
+    const iniziali = [];
+    
+    // scorriamo array[i]
+    for (let i = 0; i < array.length; i++){
+
+        // di ogni array[i] prendiamo il primo elemento [0] e lo pushamo nel array vuoto
+        iniziali.push(array[i][0]);
+
+    }
+
+    return iniziali;
+}
+
+
+
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(trovaIniziali(names));
 
 
 
